3d47dccb12db7edcf0517394d6b1e291a9bb8a95
sedes <marianorivero97@gmail.com>
2023-04-18 20:07:12 -0300

    18/04/2023

--- Vue3/main.js
+++ Vue3/main.js
@@ -7,27 +7,42 @@ const app = Vue.createApp({
     return {
       nombre:"",
       datos:null,
+      favoritos:new Map()
+    };
+  },
+
+
+  computed:{
+    esFavorito(){
+      return this.favoritos.has(this.datos.id);
     }
   },
 
+
   methods:{
     async buscar() {
-      const response = await fetch(API+this.nombre)
-      const data = await response.json()
-      this.datos = data;
-      console.log(data)
+      const response = await fetch(API+this.nombre);
+      const res = await response.json();
+      this.datos = res;
+      //console.log(res)
+    },
+
+
+    agregarFavoritos(){
+      this.favoritos.set(this.datos.id, this.datos);
+      //console.log(this.favoritos)
+    },
+
+    eliminarFavoritos(){
+      this.favoritos.delete(this.datos.id);
+      //console.log(this.favoritos)
     }
+
   }
+
 
 }).mount('#app')
 
-
-/**
-      const response = await fetch(API+this.nombre)
-      const data = await response.json()
-      this.datos = data;
-      console.log(data)
- */
 
 
 
